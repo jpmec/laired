0cca3e96ffdf16f5bc8388c00d90b9e2b0da0bac
Josh Petitt <joshpetitt@jpmec.com>
2015-11-02 21:18:53 -0600

    Added simple quests channel

---
+++ phoenix/laired/web/channels/quest_channel.ex
@@ -0,0 +1,37 @@
+defmodule Laired.QuestChannel do
+  use Phoenix.Channel
+  require Logger
+
+  def join("quests:lobby", message, socket) do
+    Process.flag(:trap_exit, true)
+    :timer.send_interval(5000, :ping)
+    send(self, {:after_join, message})
+
+    {:ok, socket}
+  end
+
+  def join("quests:" <> _private_subtopic, _message, _socket) do
+    {:error, %{reason: "unauthorized"}}
+  end
+
+  def handle_info({:after_join, msg}, socket) do
+    broadcast! socket, "hero:entered", %{hero: msg["hero"]}
+    push socket, "join", %{status: "connected"}
+    {:noreply, socket}
+  end
+
+  def handle_info(:ping, socket) do
+    push socket, "new:msg", %{hero: "SYSTEM", body: "ping"}
+    {:noreply, socket}
+  end
+
+  def terminate(reason, _socket) do
+    Logger.debug"> leave #{inspect reason}"
+    :ok
+  end
+
+  def handle_in("new:msg", msg, socket) do
+    broadcast! socket, "new:msg", %{hero: msg["hero"], body: msg["body"]}
+    {:reply, {:ok, %{msg: msg["body"]}}, assign(socket, :hero, msg["hero"])}
+  end
+end
